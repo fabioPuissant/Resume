572361f65a42a7a1da093a4622ab6396c49e436c
fabioPuissant <fabio.puissant@hotmail.com>
2020-04-18 20:29:07 +0200

    savepoint 8:29

--- src/components/BlogContent.vue
+++ src/components/BlogContent.vue
@@ -1,39 +1,78 @@
 <template>
-<div   class="mx-auto pa-6">
-<v-card :data-aos="fadeDirection"
-    max-width="400">
-        <v-img height="300px" v-bind:src="article.urlToImage"></v-img>
-        <v-container fill-height fluid>
-          <v-layout>
-            <v-flex xs12 align-end d-flex>
-              <span class="headline">{{ article.title }}</span>
-            </v-flex>
-          </v-layout>
-        </v-container>
-        <v-card-text>{{ article.description }}</v-card-text>
-        <v-card-actions>
-              <v-chip small color="secondary" class="white--text">
-                    {{article.category}}
-                  </v-chip>
-                  <v-spacer></v-spacer>
-          <v-chip small replace color="info" v-bind:href="article.url" target="_blank">{{article.date}}</v-chip>
-        </v-card-actions>
-      </v-card>
-</div>
-    
+  <div class="mx-auto pa-6 hvr-float">
+    <v-card
+      v-on:hover=""
+      :data-aos="fadeDirection"
+      max-width="400"
+      :class="{ 'on-hover': hover }"
+    >
+      <v-img height="300px" v-bind:src="article.urlToImage"></v-img>
+      <v-container fill-height fluid>
+        <v-layout>
+          <v-flex xs12 align-end d-flex>
+            <span class="headline">{{ article.title }}</span>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <v-card-text>{{ article.description }}</v-card-text>
+      <v-card-actions>
+        <v-chip small color="secondary" class="white--text">{{article.category}}</v-chip>
+        <v-spacer v-if="article.hasOwnProperty('link')"></v-spacer>
+        <v-btn
+          v-if="article.hasOwnProperty('link')"
+          icon
+          class="red--text"
+          v-on:click="redirectTo(article.link)"
+        >
+          <v-icon large>{{article.icon}}</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-chip
+          small
+          replace
+          color="info"
+          v-bind:href="article.url"
+          target="_blank"
+        >{{article.date}}</v-chip>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init({
-  duration: 1200
+  duration: 1000
 });
 
 export default {
+  data() {
+    return {
+      "hover": false,
+
+    }
+  },
   props: {
     article: Object,
-    fadeDirection: String
+    fadeDirection: String,
+  },
+  methods: {
+    redirectTo(location) {
+      window.location.replace(location);
+    }
   }
 };
 </script>
+
+<style scoped>
+
+
+.v-card:not(.on-hover) {
+  opacity: 0.6;
+}
+
+.show-btns {
+  color: rgba(255, 255, 255, 1);
+}
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,26 +1,36 @@
 <template>
-   <v-container fluid>
+  <v-container fluid>
     <v-row>
+      <v-col>
+        <v-row>
+          <v-tabs background-color="transparent" centered dark icons-and-text>
+            <v-tabs-slider></v-tabs-slider>
+
+            <v-tab v-on:click="choseBlogSubject('Seminar')">Seminar</v-tab>
+
+            <v-tab v-on:click="choseBlogSubject('International')">International</v-tab>
+
+            <v-tab v-on:click="choseBlogSubject('Event')">Event</v-tab>
+          </v-tabs>
+        </v-row>
+      </v-col>
       <v-col cols="12">
-        <v-row 
-        wrap
-        align="start"
-          justify="center"
-          >
-      <BlogContent v-for="(article, i) in articles" :key="i"
-       :article="article" 
-       :fadeDirection=" i%2 == 0 ? 'fade-right' : 'fade-left'"
-        class="mx-auto mt-8"
-        ></BlogContent>
+        <v-row wrap align="start" justify="center">
+          <BlogContent
+            v-for="(article, i) in rightArticles"
+            :key="i"
+            :article="article"
+            :fadeDirection=" i%2 == 0 ? 'fade-right' : 'fade-left'"
+            class="mx-auto mt-8"
+          ></BlogContent>
         </v-row>
       </v-col>
     </v-row>
   </v-container>
-   
 </template>
 
 <script>
-import BlogContent from '../components/BlogContent'
+import BlogContent from "../components/BlogContent";
 export default {
   name: "Blog",
   components: {
@@ -29,20 +39,27 @@ export default {
   data() {
     return {
       start: true,
-      subject: "",
-        articles: require('@/data/articles.json'),
+      subject: "Seminar",
+      articles: require("@/data/articles.json")
     };
   },
   methods: {
     choseBlogSubject(prop) {
-      this.start = !this.start;
       this.subject = prop;
     },
+    printSome(val) {
+      console.log(`${val}`);
+    }
   },
+  computed: {
+    rightArticles: function() {
+      console.log(this.chosenArticleType);
+      return this.articles.filter(t => t.category === this.subject);
+    }
+  }
 };
 </script>
 
 <style scoped>
-
 </style>
 
